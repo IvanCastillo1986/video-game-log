ee7da85587e00b26d28b671821a840490da976ef
IvanCastillo1986 <total180training@gmail.com>
2024-02-05 04:07:58 -0500

    added <PrivateRoute /> to protected routes on no user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AuthProvider } from './components/auth/AuthProvider';
 
 import Navbar from './layout/navbar/Navbar';
 import Home from './pages/home/Home';
-import ChooseConsole from './components/choose_console/ChooseConsole';
+import ChooseConsole from './pages/choose_console/ChooseConsole';
 import PC from './pages/pc/PC';
 import Nintendo from './pages/nintendo/Nintendo';
 import Sega from './pages/sega/Sega';
@@ -12,6 +12,7 @@ import AddGame from './pages/add_game/AddGame';
 import EditGame from './pages/edit_game/EditGame';
 import NotFound from './pages/not_found/NotFound';
 import UserMustSignIn from './pages/user_must_sign_in/UserMustSignIn';
+import PrivateRoutes from './components/PrivateRoutes';
 
 import './App.scss';
 
@@ -30,13 +31,17 @@ function App() {
           {/* Our Route paths are succeeded by an asterisk because we have deeper nested Routes */}
           <Routes>
             <Route index element={<Home />} />
-            <Route path="choose-console" element={<ChooseConsole />} />
+            
+            <Route element={<PrivateRoutes />}>
+              <Route path="choose-console" element={<ChooseConsole />} />
+              <Route path="pc/*" element={<PC />} />
+              <Route path="nintendo/*" element={<Nintendo />} />
+              <Route path="sega/*" element={<Sega />} />
+              <Route path="add-game" element={<AddGame />} />
+              <Route path="edit-game" element={<EditGame />} />
+            </Route>
+
             <Route path="not-signed-in" element={<UserMustSignIn />} />
-            <Route path="pc/*" element={<PC />} />
-            <Route path="nintendo/*" element={<Nintendo />} />
-            <Route path="sega/*" element={<Sega />} />
-            <Route path="add-game" element={<AddGame />} />
-            <Route path="edit-game" element={<EditGame />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
---
+++ src/components/PrivateRoutes.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import { UserContext } from './auth/AuthProvider'
+
+
+// This is different from the old <PrivateRoute /> formula before v6.
+// The <Outlet /> component allows us to pass multiple routes in, instead of wrapping
+// every component that needs a redirect inside of <PrivateRoute />
+export default function PrivateRoutes() {
+
+    const user = useContext(UserContext)
+
+    return (
+        user.email ? <Outlet /> : <Navigate to='/not-signed-in' />
+    )
+}
